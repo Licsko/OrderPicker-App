703a5065941cd321a2ac4018fc24cc3ca421f27c
Licsko <varga.sandor@mail.com>
2021-12-07 09:15:42 +0100

    pár változtatás, mobil barátabb megjelenés.

--- js/app.js
+++ js/app.js
@@ -20,29 +20,29 @@ commericalUnit_Input.addEventListener('click', function() {
 })
 
 
+const normal = 1125;
 
 
-
-
+// D3.js framework
 
 
 
 // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var margin = {top: 30, right: 30, bottom: 70, left: 40},
+    width = 350;
+    height = 320 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
+    .attr("width", width + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
 // Parse the Data
-d3.csv("https://raw.githubusercontent.com/Licsko/OrderPicker-App/main/adatok.csv", function(data) {
+d3.csv("./adatok.csv", function(data) {
 
 // X axis
 var x = d3.scaleBand()
@@ -54,14 +54,17 @@ svg.append("g")
   .call(d3.axisBottom(x))
   .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+    .style("text-anchor", "end")
+    .style("font-size", 4)
+   . style("margin-top", 2);
 
 // Add Y axis
 var y = d3.scaleLinear()
-  .domain([0, 1050])
+  .domain([0, normal])
   .range([ height, 0]);
 svg.append("g")
-  .call(d3.axisLeft(y));
+  .call(d3.axisLeft(y))
+  .attr("color", "white");
 
 // Bars
 svg.selectAll("mybar")
@@ -72,6 +75,5 @@ svg.selectAll("mybar")
     .attr("y", function(d) { return y(d.Egyseg); })
     .attr("width", x.bandwidth())
     .attr("height", function(d) { return height - y(d.Egyseg); })
-    .attr("fill", "#69b3a2")
-
+    .attr("fill", "hsl(211, 26%, 38%)")
 })
